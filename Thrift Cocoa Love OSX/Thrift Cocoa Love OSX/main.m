55fd24bab6dedb447c0522c1e0cd0df8de2a68e6
Mike Riley <mikeriley@yelirekim.com>
2015-03-15 05:26:17 -0400

    ignores

---
+++ Thrift Cocoa Love OSX/Thrift Cocoa Love OSX/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Thrift Cocoa Love OSX
+//
+//  Created by NoWait on 3/15/15.
+//  Copyright (c) 2015 NoWait. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ thrift-cocoa-love/thrift-cocoa-love/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  thrift-cocoa-love
+//
+//  Created by NoWait on 3/15/15.
+//  Copyright (c) 2015 NoWait. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
